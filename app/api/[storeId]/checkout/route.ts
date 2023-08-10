5b2d2b86e94c5c189c2cba024b1a38b7be845282
George <xsova113@gmail.com>
2023-08-09 18:17:10 -0700

    minor update

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -62,8 +62,8 @@ export async function POST(
     phone_number_collection: {
       enabled: true,
     },
-    success_url: `${process.env.FRONTEND_STORE_URL || process.env.FRONTEND_GYMRATS_URL}/cart?success=1`,
-    cancel_url: `${process.env.FRONTEND_STORE_URL || process.env.FRONTEND_GYMRATS_URL}/cart?canceled=1`,
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
     metadata: {
       orderId: order.id,
     },
